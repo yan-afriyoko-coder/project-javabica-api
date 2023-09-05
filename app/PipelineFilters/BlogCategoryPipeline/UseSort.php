96ccbeec64e5a8c533dc6e31dc63c911d39e730c
Yan Afriyoko <mail.yanafriyoko@gmail.com>
2023-09-05 14:35:06 +0700

    sort category ascending

--- app/PipelineFilters/BlogCategoryPipeline/UseSort.php
+++ app/PipelineFilters/BlogCategoryPipeline/UseSort.php
@@ -13,7 +13,7 @@ class UseSort
     if(request()->has('sort_type')){
         $query->orderBy('created_at',request()->get('sort_type'));
     }else{
-        $query->orderBy('created_at','desc');
+        $query->orderBy('created_at','asc');
     }
     
     return $next($query);
