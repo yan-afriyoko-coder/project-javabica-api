a84bb409ede37236946a6cfc8a6a5b313369c7cd
Yan Afriyoko <mail.yanafriyoko@gmail.com>
2023-09-05 15:17:13 +0700

    filter blog

--- app/PipelineFilters/BlogPipeline/GetByWord.php
+++ app/PipelineFilters/BlogPipeline/GetByWord.php
@@ -9,20 +9,29 @@ class GetByWord
 
   public function handle($query, Closure $next)
   {
-    if (request()->has('keyword') )
-    {
-      
         $keyword = request()->get('keyword');
+        $category = request()->get('category_id');
 
-        if($keyword) {
-          
-          $query->where(function ($query)use ($keyword)  {
-              $query->where('title','like', '%' .$keyword.'%');
+        if($keyword && $category) 
+        {
+          $query->where(function ($query)use ($keyword, $category)  {
+              $query->where('title','like', '%' .$keyword.'%')->where('fk_category', $category);
           });
           
         }
-      
-    }
+        elseif($keyword && !$category)
+        {
+            $query->where(function ($query)use ($keyword)  {
+                $query->where('title','like', '%' .$keyword.'%');
+            });
+        }
+        elseif(!$keyword && $category)
+        {
+            $query->where(function ($query)use ($category)  {
+                $query->where('fk_category', $category);
+            });
+        }
+
 
     return $next($query);
   }
