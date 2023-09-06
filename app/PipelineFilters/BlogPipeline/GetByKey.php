6b6a840d8d76ceb9c5c247f0a20033d924b6f331
Yan Afriyoko <mail.yanafriyoko@gmail.com>
2023-09-06 16:45:29 +0700

    update api blog

--- app/PipelineFilters/BlogPipeline/GetByKey.php
+++ app/PipelineFilters/BlogPipeline/GetByKey.php
@@ -10,6 +10,10 @@ class GetByKey
   public function handle($query, Closure $next)
   { 
 
+      if (request()->has('by_id') && request()->get('by_id'))
+      {
+          $query->where('id', request()->get('by_id'));
+      }
       if (request()->has('slug') && request()->get('slug'))
       {
           $query->where('slug', request()->get('slug'));
