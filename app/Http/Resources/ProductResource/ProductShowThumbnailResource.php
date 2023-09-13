7508f41c46006b9941e4841ce4875f49d1bc6877
Yan Afriyoko <mail.yanafriyoko@gmail.com>
2023-09-13 14:04:03 +0700

    Add variant list on product public

--- app/Http/Resources/ProductResource/ProductShowThumbnailResource.php
+++ app/Http/Resources/ProductResource/ProductShowThumbnailResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\ProductResource;
 
+use App\Models\Product_variant;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductShowThumbnailResource extends JsonResource
@@ -28,6 +29,119 @@ class ProductShowThumbnailResource extends JsonResource
         } else {
 
             $image = null;
+        }
+        //== variant
+        if (count($this->resource['data']->hasMany_variantActive) > 0) 
+        {    
+          
+            $type_parent =   Product_variant::select('fk_attribute_parent_id')->where('fk_product_id', $this->resource['data']->id)->where('status','ACTIVE')->groupby('fk_attribute_parent_id')->first();
+            $type_child  =   Product_variant::select('fk_attribute_child_id')->where('fk_product_id', $this->resource['data']->id)->where('status','ACTIVE')->groupby('fk_attribute_child_id')->first();
+            $listParent  =   Product_variant::select('fk_attribute_parent_id')->where('fk_product_id', $this->resource['data']->id)->where('status','ACTIVE')->groupby('fk_attribute_parent_id')->get();
+           
+            //** HANDLER APABILA TIDAK MEMILIKI VARIANT  */
+            if ($type_parent->fk_attribute_parent_id == null &&  $type_child->fk_attribute_child_id == null) 
+            { 
+               
+                $variantList = $this->resource['data']->hasMany_variantActive->map(function ($query) {
+                  
+                    $getPrice = $this->recalculatePrice($query->price,$query->discount);
+
+                    return array(
+                        'parent' => null,
+                        'child' => [[
+                            'id'        => $query->id,
+                            'name'      => null,
+                            'sku'       => $query->sku,
+                            'stock'       => $query->stock,
+                            'is_ignore_stock'       => $query->is_ignore_stock,
+                            'price'     => $getPrice,
+                        ]]
+                    );
+                });
+            } else {
+                
+                $variantList =  $listParent->map(function ($query) {
+                  
+                    $child =   Product_variant::where('fk_product_id', $this->resource['data']->id)->where('fk_attribute_parent_id', $query->fk_attribute_parent_id)->where('fk_attribute_child_id','!=',null)->where('status','ACTIVE')->orderBy('price','asc')->get();
+                   
+                     //** HANDLER APABILA  MEMILIKI VARIANT 2 LEVEL  */
+                    if (count($child) > 0)
+                    {
+                        
+                        $getChild =  $child->map(function ($queryChild)   {
+                           
+
+                            $getPrice = $this->recalculatePrice($queryChild->price,$queryChild->discount);
+                            
+                            return array(
+                                'id'        => $queryChild->id,
+                                'sku'       => $queryChild->sku,
+                                'stock'       => $queryChild->stock,
+                                'is_ignore_stock'       => $queryChild->is_ignore_stock,
+                                'name'      => $queryChild->fk_attribute_child['taxonomy_name'],
+                                'price'     => $getPrice
+                            );
+
+                        });
+                    
+                        return array(
+                            'parent'      => $query->fk_attribute_parent->taxonomy_name,
+                            'child'       =>  $getChild,
+                        );
+
+                    } 
+                    else //** HANDLER APABILA  MEMILIKI VARIANT 1 LEVEL  */
+                    {
+                    
+                        $getPrice  =   Product_variant::select('*')->where('fk_product_id', $this->resource['data']->id)->where('fk_attribute_parent_id',$query->fk_attribute_parent_id)->where('status','ACTIVE')->orderBy('price','asc')->first();
+                     
+                          $getPriceData = $this->recalculatePrice($getPrice->price,$getPrice->discount);
+
+                            return array(
+                                'parent'      => $query->fk_attribute_parent->taxonomy_name,
+                                'child'       => [[
+                                    'id'        => $getPrice->id,
+                                    'name'    => $getPrice->fk_attribute_child_id?->taxonomy_name,
+                                    'sku'     => $getPrice->sku,
+                                    'stock'   => $getPrice->stock,
+                                    'price'   => $getPriceData,
+                                    'is_ignore_stock'       => $getPrice->is_ignore_stock,
+                                 
+                                ]]
+                            );
+                       
+                    }
+                });
+            }
+          
+            if($type_parent->fk_attribute_parent == null && $type_child->fk_attribute_child == null)
+            {
+                $variantCriteria = 'NO_VARIANT';
+            }
+            if($type_parent->fk_attribute_parent?->taxoParent['taxonomy_name'] != null)
+            {
+                $variantCriteria = 'VARIANT_LEVEL_1';
+            }
+            if($type_child->fk_attribute_child?->taxoParent['taxonomy_name'] != null)
+            {
+               
+                $variantCriteria = 'VARIANT_LEVEL_2';
+            }
+            
+            
+               
+                $variantAll = array(
+                    'variant_criteria'  => $variantCriteria,
+                    'type_parent'       => $type_parent->fk_attribute_parent?->taxoParent['taxonomy_name'],
+                    'type_child'        => $type_child->fk_attribute_child?->taxoParent['taxonomy_name'],
+                    'list'              => $variantList
+                );
+            
+           
+
+        } else {
+
+            $variantAll = null;
         }
         //==
         if ($this->resource['data']->hasMany_category != null) {
@@ -105,6 +219,36 @@ class ProductShowThumbnailResource extends JsonResource
             'image'                             => $image,
             'category'                          => $category,
             'variant_price'                     => $variant,
+            'variant_list'                      => $variantAll,
         ];
+    }
+    
+    private function recalculatePrice($pricePerItem,$discountPerItem) {
+        
+        if ($discountPerItem == null || $discountPerItem <= 0) {
+
+            $price = array(
+                'purchase_price'      => $pricePerItem,
+                'normal_price'        => $pricePerItem,
+                'discount'            => null,
+                'discount_persentage' => null,
+                'discount_margin'     => null
+            );
+        } else {
+
+            $discount_margin     =  $pricePerItem - $discountPerItem;
+            $discount_persentage =  $pricePerItem / $discountPerItem;
+            $getPersentage       =  100-(100/$discount_persentage);
+
+            $price = array(
+                'purchase_price'      => $discountPerItem,
+                'normal_price'        => $pricePerItem,
+                'discount'            => $discountPerItem,
+                'discount_persentage' => round($getPersentage),
+                'discount_margin'     => $discount_margin
+            );
+        }
+
+        return $price;
     }
 }
