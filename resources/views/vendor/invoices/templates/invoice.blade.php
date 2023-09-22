bf6a23a554c7ecc2c5df6cdfa6d48e706dd2b913
Yan Afriyoko <mail.yanafriyoko@gmail.com>
2023-09-22 14:02:36 +0700

    debug invoice blade

--- resources/views/vendor/invoices/templates/invoice.blade.php
+++ resources/views/vendor/invoices/templates/invoice.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>{{ $invoice->name }}</title>
+        <title></title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
         <style type="text/css" media="screen">
