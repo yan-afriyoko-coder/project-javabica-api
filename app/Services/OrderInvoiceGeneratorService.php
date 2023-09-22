ee06e4c53871c72961064f694c7b5f0399421350
Yan Afriyoko <mail.yanafriyoko@gmail.com>
2023-09-22 10:09:01 +0700

    debug invoice

--- app/Services/OrderInvoiceGeneratorService.php
+++ app/Services/OrderInvoiceGeneratorService.php
@@ -16,83 +16,89 @@ class OrderInvoiceGeneratorService
 {
     public function generate($dataCollection)
     {
-        $companyDetail = new Party([
-            'coporate_name'        => ''.config('javabica.corporate_name').'',
-            'address'              => ''.config('javabica.address').'',
-            'phone'                => ''.config('javabica.phone').'',
-            'email'                => ''.config('javabica.email').'',
-            'website'              => ''.config('javabica.website').'',
+        try {
+            $companyDetail = new Party([
+                'coporate_name'        => ''.config('javabica.corporate_name').'',
+                'address'              => ''.config('javabica.address').'',
+                'phone'                => ''.config('javabica.phone').'',
+                'email'                => ''.config('javabica.email').'',
+                'website'              => ''.config('javabica.website').'',
+                
+            ]);
+
+            $customer = new Party([
+                'name'          => $dataCollection->billing_first_name . ' ' . $dataCollection->billing_last_name,
+                'custom_fields' => [
+                    'Alamat' => $dataCollection->billing_address . ', ' . $dataCollection->billing_city . ', ' . $dataCollection->billing_province . ', ' . $dataCollection->billing_postal_code . ', ' . $dataCollection->billing_country . '.',
+                    'Telp' => $dataCollection->contact_billing_phone,
+                    // 'email' => $dataCollection->contact_email,
+                ],
+            ]);
+
+            $items = [];
+        
+            foreach ($dataCollection->product_order as $cart) {
+                $listCart =   (new InvoiceItem())
+                    ->title($cart->product_name)
+                    ->description(''.$cart->variant_description.'')
+                    ->quantity($cart->qty);
+                    $listCart->product_code = $cart->sku;
+                    $listCart->note         = $cart->note;
+
+    
+                if ($cart->discount_price != null || $cart->discount_price > 0) {
+
+                    $listCart->sub_total_price = $cart->qty*$cart->discount_price;
+                    $reformat                  =  $cart->qty*($cart->acctual_price-$cart->discount_price);
+                    $listCart                  = $listCart->pricePerUnit($cart->acctual_price);
+                    $listCart                  = $listCart->discount($reformat);
+                }
+
+                else
+                {
+                    $listCart=$listCart->pricePerUnit($cart->purchase_price);
+                }
+
+                array_push($items, $listCart);
+            }
+
+            $notes = [
+                '',
+                '',
+                '' . $dataCollection->invoice_note . '',
+
+            ];
+            $notes = implode("<br>", $notes);
+            dd($dataCollection, $customer, $items, $notes);
+            $invoice = Invoice::make('Invoice')
+                ->template('invoice')
+                ->status($dataCollection->payment_status)
+                ->serialNumberFormat($dataCollection->order_number)
+                ->buyer($customer)
+                ->date(Carbon::parse($dataCollection->created_at))
+                ->dateFormat('d/m/Y')
+                ->currencySymbol('Rp')
+                ->currencyFormat('{SYMBOL}{VALUE}')
+                ->currencyThousandsSeparator('.')
+                ->currencyDecimalPoint(',')
+                ->filename($customer->name.'_'.$dataCollection->order_number)
+                ->addItems($items)
+                ->notes($notes)
+                ->logo(public_path('brand/logo_blue.png'));
+                // You can additionally save generated invoice to configured disk
+                //  ->save('public');
+                //  $link = $invoice->url();
+        
+                $invoice->shipping_amount =  $dataCollection->courier_cost;
+                $invoice->companyDetail   =  $companyDetail;
             
-        ]);
-
-        $customer = new Party([
-            'name'          => $dataCollection->billing_first_name . ' ' . $dataCollection->billing_last_name,
-            'custom_fields' => [
-                'Alamat' => $dataCollection->billing_address . ', ' . $dataCollection->billing_city . ', ' . $dataCollection->billing_province . ', ' . $dataCollection->billing_postal_code . ', ' . $dataCollection->billing_country . '.',
-                'Telp' => $dataCollection->contact_billing_phone,
-                // 'email' => $dataCollection->contact_email,
-            ],
-        ]);
-
-        $items = [];
-     
-        foreach ($dataCollection->product_order as $cart) {
-            $listCart =   (new InvoiceItem())
-                ->title($cart->product_name)
-                ->description(''.$cart->variant_description.'')
-                ->quantity($cart->qty);
-                $listCart->product_code = $cart->sku;
-                $listCart->note         = $cart->note;
-
-   
-            if ($cart->discount_price != null || $cart->discount_price > 0) {
-
-                $listCart->sub_total_price = $cart->qty*$cart->discount_price;
-                $reformat                  =  $cart->qty*($cart->acctual_price-$cart->discount_price);
-                $listCart                  = $listCart->pricePerUnit($cart->acctual_price);
-                $listCart                  = $listCart->discount($reformat);
-            }
-
-            else
-            {
-                $listCart=$listCart->pricePerUnit($cart->purchase_price);
-            }
-
-            array_push($items, $listCart);
+                // dd($invoice->stream());
+                // And return invoice itself to browser or have a different view
+                return $invoice->stream();
+                
+        } catch (\Exception $e) {
+            // Tangani error, misalnya dengan mencetak pesan error
+            echo "Terjadi kesalahan saat menampilkan invoice: " . $e->getMessage();
         }
-
-        $notes = [
-            '',
-            '',
-            '' . $dataCollection->invoice_note . '',
-
-        ];
-        $notes = implode("<br>", $notes);
-
-        $invoice = Invoice::make('Invoice')
-            ->template('invoice')
-            ->status($dataCollection->payment_status)
-            ->serialNumberFormat($dataCollection->order_number)
-            ->buyer($customer)
-            // ->date(Carbon::parse($dataCollection->created_at))
-            // ->dateFormat('d/m/Y')
-            // ->currencySymbol('Rp')
-            // ->currencyFormat('{SYMBOL}{VALUE}')
-            // ->currencyThousandsSeparator('.')
-            // ->currencyDecimalPoint(',')
-            // ->filename($customer->name.'_'.$dataCollection->order_number)
-            ->addItems($items)
-            // ->notes($notes)
-            ->logo(public_path('brand/logo_blue.png'));
-            // You can additionally save generated invoice to configured disk
-            //  ->save('public');
-            //  $link = $invoice->url();
-      
-            $invoice->shipping_amount =  $dataCollection->courier_cost;
-            $invoice->companyDetail   =  $companyDetail;
-         
-            dd($invoice->stream());
-            // And return invoice itself to browser or have a different view
-            return $invoice->stream();
     }
 }
