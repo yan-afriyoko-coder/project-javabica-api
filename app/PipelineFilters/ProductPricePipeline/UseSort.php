7742168bbb1c4e82636c85a947d069401f17e12a
Yan Afriyoko <mail.yanafriyoko@gmail.com>
2023-11-04 10:12:07 +0700

    sort wholesale product price

--- app/PipelineFilters/ProductPricePipeline/UseSort.php
+++ app/PipelineFilters/ProductPricePipeline/UseSort.php
@@ -9,9 +9,9 @@ class UseSort
 
   public function handle($query, Closure $next)
   {
-   
+
     if(request()->get('sort_by'))
-    { 
+    {
         if(request()->has('sort_type'))
         {
           $sort_type = request()->get('sort_type');
@@ -20,14 +20,14 @@ class UseSort
         {
           $sort_type = 'desc';
         }
-        
+
         //  $query->orderBy(''.request()->get('sort_by').'',$sort_type);
-         $query->orderBy('id','desc');
+         $query->orderBy('id','asc');
     }
     else  {
       $query->orderBy('created_at','desc');
     }
-    
+
     return $next($query);
   }
 }
